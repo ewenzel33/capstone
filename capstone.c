136ba40deeddde1b116230ed23279648b9bb76d7
ewenzel33 <35603996+ewenzel33@users.noreply.github.com>
2020-02-27 15:26:49 -0500

    Add files via upload

--- capstone.c
+++ capstone.c
@@ -9,16 +9,17 @@ extern "C"{
 #endif
 
 typedef struct rtlsdr_dev rtlsdr_dev_t;
-char manufact[64];
-char product[64];
-char serial[64];
+char manufact[256];
+char product[256];
+char serial[256];
 int ret;
-uint32_t freq;
+int ret1;
+int freq;
 int len;
 int nread;
 uint32_t rate;
 rtlsdr_dev_t *dptr;
-char buffer[256];
+unsigned char buffer[4096];
 int main(int argc, char *argv[]){
 
 int ret=rtlsdr_get_device_usb_strings(0,manufact,product,serial);
@@ -40,7 +41,7 @@ fprintf(stderr, "open function not completed\n");
 exit(1);
 }
 
-freq=991000000;
+freq=1079000000;
 
 ret=rtlsdr_set_center_freq(dptr,freq);
 if (ret==0){
@@ -51,28 +52,54 @@ fprintf(stderr, "freq set failed\n");
 exit(1);
 }
 
+ret=rtlsdr_reset_buffer(dptr);
+if (ret !=0){
+fprintf(stderr, "Failed to reset internal buffer\n");
+}
+else {
+fprintf(stderr, "Buffer Reset\n");
+}
+
 rate=2000000;
 
 ret=rtlsdr_set_sample_rate(dptr,rate);
-if (ret==0){
+
+if (ret !=0){
 fprintf(stderr, "Failed to set sample rate\n");
 }
-
-ret=rtlsdr_reset_buffer(dptr);
-if (ret==0){
-fprintf(stderr, "Failed to reset internal buffer\n");
-exit(1);
+else {
+fprintf(stderr, "Sample Rate Set\n");
 }
 unsigned char data[16384];
-len=sizeof(data);
+len=16384;
 
-ret=rtlsdr_read_sync(dptr,buffer,len,&nread);
-if (ret==0){
-printf("complete\n");
+ret1=rtlsdr_read_sync(dptr,data,len,&nread);
+
+printf("# read_sync	=> %d (%d bytes)\n", ret1, nread);
+fprintf(stderr,"ret1 is equal to %d\n",ret1);
+fprintf(stderr,"nread is equal to %d\n",nread);
+fprintf(stderr,"len is equal to %d\n",len);
+fprintf(stderr,"data is equal to %d\n",data);
+fprintf(stderr,"dptr is equal to %d\n",dptr);
+
+int num=2129691224;
+fprintf(stderr,"num is equal to %d\n",num);
+int i;
+float sum, mean, sd, var;
+for(i=0;i<16384;i++){
+sum=ret1;
+mean=sum/num;
+sum=0.0;
 }
-else {
-printf("READ FAILED\n");
+for(i=0;i<16384;i++){
+var=(num-mean)*(num-mean);
+sd=sqrt(var/num);
 }
+fprintf(stderr,"sum is equal to %d\n",var);
+fprintf(stderr,"mean is equal to %d\n",mean);
+fprintf(stderr,"var is equal to %d\n",var);
+fprintf(stderr,"sd is equal to %d\n",sd);
+return rtlsdr_close(dptr);
 return 0;
 }
 
